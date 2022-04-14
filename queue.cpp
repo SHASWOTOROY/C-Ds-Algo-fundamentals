c1246fe231ffc225e51b2b391d169d4878f17bbf
SHASWOTOROY <shaswata2015@gmil.com>
2022-04-15 03:17:46 +0600

    DSA TOPIC

---
+++ queue.cpp
@@ -0,0 +1,75 @@
+
+#include <iostream>
+using namespace std;
+# define n 100
+class queue
+{
+    int *arr;
+    int front;
+    int back;
+public:
+    queue()
+    {
+        arr= new int[n];
+        front=-1;
+        back=-1;
+
+    }
+
+    void push(int x)
+    {
+
+        if(back==n-1)
+        {
+            cout<<"over flow"<<endl;
+            return;
+        }
+        back++;
+        arr[back]=x;
+        if(front==-1)
+        {
+            front++;
+        }
+    }
+
+    void pop()
+    {
+
+        if(front==-1||front>back)
+        {
+            cout<<"no elemnt to insert"<<endl;
+            return;
+        }
+        front++;
+    }
+    int peek()
+    {
+
+        if(front==-1||front>back)
+        {
+            cout<<"no elemnt to insert"<<endl;
+            return-1;
+        }
+        return arr[front];
+    }
+    bool empty()
+    {
+
+        if(front==-1||front>back)
+        {
+            cout<<"no elemnt to insert"<<endl;
+            return true;
+        }
+        return false;
+    }
+};
+int main()
+{
+    queue q;
+    q.push(3);
+    q.push(4);
+    cout<<q.peek()<<endl;
+    q.pop();
+    cout<<q.peek()<<endl;
+
+}
