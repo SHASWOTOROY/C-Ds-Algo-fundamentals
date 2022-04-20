7f07c025953dc9b10f56582f2b7f91bc1f1c134f
Shaswata Roy <63513936+SHASWOTOROY@users.noreply.github.com>
2022-04-20 21:33:26 +0600

    Add files via upload
    stackstringreverse

---
+++ stringreverseusingstack.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void reversestring(string s)
+{
+    stack<string>st;
+
+    for(int i=0; i<s.length(); i++)
+    {
+        string word="";
+        while(s[i]!=' '&& i<s.length())
+        {
+            word=word+s[i];
+            i++;
+        }
+        st.push(word);
+    }
+    while(!st.empty())
+    {
+        cout<<st.top()<<" ";
+        st.pop();
+
+    }
+    cout<<endl;
+}
+
+int main()
+{
+
+    string s;
+    getline(cin,s);
+    reversestring(s);
+}
